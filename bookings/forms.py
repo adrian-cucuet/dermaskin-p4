5e1ce2974b9431f239cc4077795ae0c37808114e
adrian-cucuet <adrian.cucuet@hotmail.com>
2023-08-05 21:10:55 +0000

    Added validation error for date to accept only future dates on booking form

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Booking
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 class DateInput(forms.DateInput):
@@ -8,6 +10,12 @@ class DateInput(forms.DateInput):
 
 
 class BookingForm(forms.ModelForm):
+    def clean_date(self):
+        date = self.cleaned_data['date']
+        if date < timezone.now().date():
+            raise forms.ValidationError(message='Date cannot be in the past')
+        return date
+
     class Meta:
         model = Booking
         fields = '__all__'
